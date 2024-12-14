7f1f294ec9895feba76d86474f1b527bb2df628f
yusuftahademirer <156345766+yusuftahademirer@users.noreply.github.com>
2024-12-14 17:23:05 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import HomeScreen from './screens/HomeScreen';
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <HomeScreen></HomeScreen>
+    </NavigationContainer>
+  );
+}
